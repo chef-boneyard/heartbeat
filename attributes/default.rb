7c812c13cd9c0520eede105baf1928947be669d5
Justin Witrick <justin.witrick@mailtrust.com>
2012-09-24 08:47:49 -0400

    changed the recipe to use local variables for package list, instead of setting a node attribute

--- attributes/default.rb
+++
@@ -1,7 +0,0 @@
-
-case node['platform']
-when "centos", "redhat"
-    node['heartbeat']['packages'] = ['heartbeat', 'heartbeat-devel']
-when "ubuntu", "debian"
-    node['heartbeat']['packages'] = ['heartbeat', 'heartbeat-dev']
-end
