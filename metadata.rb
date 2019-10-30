02887a37c88070029625774412f079e8e4ee036c
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:07:14 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs but does not configure heartbeat'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '3.0.0'
-
-recipe 'heartbeat', 'Installs, but does not configure, heartbeat'
 
 %w( debian ubuntu centos redhat amazon scientific oracle ).each do |os|
   supports os
